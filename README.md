5557780f123d250d8a2e017ef8adf486eee318a8
sthurston99 <thurst27@students.rowan.edu>
2022-02-24 23:11:54 -0500

    Initial Commit, contains example python file and README

---
+++ README.md
@@ -0,0 +1,20 @@
+# This is a README File
+
+This one doesn't have much to say, but when you add one to your repository, it'll show up on the webpage in big, bold letters.
+
+It's formatted using Markdown, a pretty simple text formatting language that you're probably familiar with if you use Discord. There's *italics*, **bold**, and ***bold italics***, plus `code blocks` and ~~strikethrough~~,  all kinds of things.
+
+## You can set headers and subheaders pretty easily for organization, too.
+
+* You can even do
+* Lists and things
+	* That even support indenting!
+	* Pretty neat.
+
+> There's your classic blockquotes.
+
+[And even ways to embed links](https://www.youtube.com/watch?v=NjZDwJGN82s)
+
+That's most of it, but there's some handy cheat-sheets, guides, and editors out there you can use if you ever get confused.
+
+## Learning markdown is important, because having a nice, pretty, readable README is the first step to having someone be able to clone and compile your code for their own use.
---
+++ example.py
@@ -0,0 +1,25 @@
+# This is a Python file!
+# In case you didn't know, Python's a pretty popular programming language.
+# You probably already have the Python interpreter installed on your computer,
+# and you didn't even know it! That's how ubiquitous it is.
+
+# In fact, if you attended the earlier Intro to Programming workshop,
+# you should know a little bit about Python!
+
+# But if you didn't, and you don't, don't worry:
+# This is a simple Python script that just prints some words to the console.
+# It checks whether or not a variable is a certain vailue, and will print
+# one of two strings of text depending on whether it is, or isn't!
+
+# Feel free to tamper with it and make some changes, or add your own stuff!
+# It's not the most advanced code file in the world, but it's enough to show
+# how git will track your changes.
+
+num = 9
+
+print("I'm going to check the num variable for the number 9.\n")
+
+if num == 9:
+	print("Hey! The num variable is 9! That's good news for some reason.")
+else:
+	print("Hmm, turns out that num was actually " + str(num) + ".")
